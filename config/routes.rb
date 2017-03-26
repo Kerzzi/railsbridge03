6d923e6b49f37f1d8a9ddfad9f79c2595f011c44
Kerzzi <kerzzi@outlook.com>
2017-03-26 20:13:35 +0800

    3-13加分题实作

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+  end
+  root 'topics#index'
+  get 'about' => 'topics#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
